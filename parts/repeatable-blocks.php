4be7158e8730f007e900c389dfa010e54cac97e0
Lisa <lisaqdebona@gmail.com>
2024-04-19 03:25:24 -0400

    styling About Us page

--- parts/repeatable-blocks.php
+++ parts/repeatable-blocks.php
@@ -138,6 +138,73 @@
       <?php } ?>
     <?php } 
 
+
+    else if( get_row_layout() == 'two_column_slant_alltext' ) { 
+      $column1_width = get_sub_field('column1_width');
+      $column2_width = get_sub_field('column2_width');
+      $width1 = ($column1_width) ? 'width:'.$column1_width.'%':'';
+      $width2 = ($column2_width) ? 'width:'.$column2_width.'%':'';
+
+      //Column 1
+      $icon = get_sub_field('icon'); 
+      $text = get_sub_field('textcontent'); 
+      $button = get_sub_field('button'); 
+      $btnLink = (isset($button['url']) && $button['url']) ? $button['url'] : '';
+      $btnTitle = (isset($button['title']) && $button['title']) ? $button['title'] : '';
+      $btnTarget = (isset($button['target']) && $button['target']) ? $button['target'] : '_self';
+      $button_bgcolor = get_sub_field('button_bgcolor'); 
+      $textcolor = (get_sub_field('textcolor')) ? get_sub_field('textcolor') : '#FFF'; 
+      $bgcolor = (get_sub_field('bgcolor')) ? get_sub_field('bgcolor') : '#CCC'; 
+
+      //Column 2
+      $text2 = get_sub_field('textcontent_col2'); 
+      $button2 = get_sub_field('button_col2'); 
+      $btnLink2 = (isset($button2['url']) && $button2['url']) ? $button2['url'] : '';
+      $btnTitle2 = (isset($button2['title']) && $button2['title']) ? $button2['title'] : '';
+      $btnTarget2 = (isset($button2['target']) && $button2['target']) ? $button2['target'] : '_self';
+      $button_bgcolor2 = get_sub_field('button_bgcolor_col2'); 
+      $textcolor2 = (get_sub_field('textcolor_col2')) ? get_sub_field('textcolor_col2') : '#808184'; 
+      $bgcolor2 = (get_sub_field('bgcolor_col2')) ? get_sub_field('bgcolor_col2') : '#FFF'; 
+
+      $section_class = ($text && $text2) ? 'half':'full';
+
+      
+
+
+      if( $text || $text2 ) { ?>
+      <section id="two_column_slant_alltext_<?php echo $i?>" class="repeatable_section section_two_column_slant_alltext <?php echo $section_class?>">
+        <div class="columns">
+          <?php if ($text) { ?>
+          <div class="textcol column1" style="color:<?php echo $textcolor ?>;<?php echo $width1; ?>">
+            <span class="bgcolor" style="background-color:<?php echo $bgcolor ?>;"></span>
+            <span class="slant" style="background-color:<?php echo $bgcolor ?>;"></span>
+            <div class="inner">
+              <?php echo $text ?>
+              <?php if($btnLink  && $btnTitle) { ?>
+              <div class="button-wrap">
+                <a href="<?php echo $btnLink ?>" target="<?php echo $btnTarget ?>" class="button <?php echo $button_bgcolor; ?>"><?php echo $btnTitle ?></a>
+              </div>
+              <?php } ?>
+            </div>
+          </div>
+          <?php } ?>
+          <?php if ($text2) { ?>
+          <div class="textcol column2" style="background-color:<?php echo $bgcolor2 ?>;color:<?php echo $textcolor2 ?>;<?php echo $width2; ?>">
+            <div class="inner">
+              <?php echo $text2 ?>
+              <?php if($btnLink2  && $btnTitle2) { ?>
+              <div class="button-wrap">
+                <a href="<?php echo $btnLink2 ?>" target="<?php echo $btnTarget2 ?>" class="button <?php echo $button_bgcolor2; ?>"><?php echo $btnTitle2 ?></a>
+              </div>
+              <?php } ?>
+            </div>
+          </div>
+          <?php } ?>
+        </div>
+      </section>
+      <?php } ?>
+    <?php } 
+
     else if( get_row_layout() == 'fullwidth_content' ) { 
       $icon = get_sub_field('icon'); 
       $text = get_sub_field('textcontent'); 
